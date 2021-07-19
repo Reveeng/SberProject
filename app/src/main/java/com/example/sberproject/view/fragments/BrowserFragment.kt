7a5a10125b1d2df0fc109851871aaf1ee0340713
evenmonk <zh.vibe18@yandex.ru>
2021-07-19 19:13:29 +0500

    .

--- app/src/main/java/com/example/sberproject/view/fragments/BrowserFragment.kt
+++ app/src/main/java/com/example/sberproject/view/fragments/BrowserFragment.kt
@@ -1,4 +1,35 @@
 package com.example.sberproject.view.fragments
 
-class BrowserFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.sberproject.databinding.FragmentBrowserBinding
+
+
+class BrowserFragment : Fragment() {
+    private var _binding: FragmentBrowserBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentBrowserBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        // Inflate the layout for this fragment
+        return root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.webView.loadUrl(arguments?.getString("stat")!!)
+    }
 }
